288c4f27bae65d937e623c89fe23db12dcd88733
Efremov AV <ekspil@yandex.ru>
2024-03-11 14:30:49 +1000

    1.0.64

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -46,7 +46,7 @@
             </div>
           </v-card-title>
 
-          <v-container fluid v-if="productDescription.product.helpers.length > 0">
+          <v-container fluid v-if="productDescription.product && productDescription.product.helpers && productDescription.product.helpers.length > 0">
             <v-row>
               <v-col cols="12"
               ><div class="align-self-center">
